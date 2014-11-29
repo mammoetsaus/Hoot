0ae19cd34b313629cbb3592d731c7aca9ced6bff
Thomas Voet <thomas.voet@live.be>
2014-11-29 19:53:01 +0100

    Turn server tryout

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,4 +9,19 @@ fabHomeLogin.onclick =  function(e) {
     }
 };
 
-console.log(fabHomeLogin);
+getTurnServer("https://computeengineondemand.appspot.com/turn?username=hoot&key=1830");
+
+
+function getTurnServer(turn_url) {
+    console.log('Getting TURN server from ', turn_url);
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var turnServer = JSON.parse(xhr.responseText);
+            console.log('Got TURN server: ', turnServer);
+            console.log(turnServer.username + " + + + " + turnServer.turn  + " / / / " + turnServer.password);
+        }
+    };
+    xhr.open('GET', turn_url, true);
+    xhr.send();
+}
